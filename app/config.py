07648a75b26b389859a30e73b494dffc4afe959c
Fabrizio Miano <fabriziomiano@gmail.com>
2021-02-05 18:31:04 +0100

    Solve Ex 3a (#7)
    Add app backbone, models, blueprint, and API endpoint

---
+++ app/config.py
@@ -0,0 +1,13 @@
+"""
+Configuration file
+"""
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+if not SQLALCHEMY_DATABASE_URI:
+    raise Exception("Database URI not specified")
+SQLALCHEMY_TRACK_MODIFICATIONS = True
