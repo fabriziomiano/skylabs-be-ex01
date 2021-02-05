97e48db531e3557ebc02bbc62111788d8fa097ce
Fabrizio Miano <fabriziomiano@gmail.com>
2021-02-05 19:11:38 +0100

    Solve Ex 4 (#9)
    * Add /csv endpoint
    * Update README.md

--- app/config.py
+++ app/config.py
@@ -11,3 +11,4 @@ SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 if not SQLALCHEMY_DATABASE_URI:
     raise Exception("Database URI not specified")
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+ALL_RECORDS_CSV_FILENAME = "records.csv"
