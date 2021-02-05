dfca792a1d1fb849487e54e1b71eb3c2fc4e7091
Fabrizio Miano <fabriziomiano@gmail.com>
2021-02-05 21:15:44 +0100

    Fix Solution Ex 3b (#4)
    * Handle JSON body when receiving POST
    * Add JSON schema validation
    * Update requirements.txt

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,6 +3,16 @@ API Blueprint
 """
 from flask import Blueprint
 
+STATS_SCHEMA = {
+    "type": "object",
+    "required": ["aggregationType", "aggregationValue"],
+    "properties": {
+        "aggregationType": {"type": "string"},
+        "aggregationValue": {"type": "integer"},
+    },
+}
+ACCEPTED_AGGREGATION_TYPES = ("age", "education_level_id", "occupation_id")
+
 api = Blueprint("api", __name__, url_prefix='/api')
 
 from . import endpoints
