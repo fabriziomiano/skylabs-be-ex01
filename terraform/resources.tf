dde00fcc740cc44038bf6147d9316886ecb40174
Fabrizio Miano <fabriziomiano@gmail.com>
2021-02-10 01:10:17 +0100

    Add terraform configuration

---
+++ terraform/resources.tf
@@ -0,0 +1,22 @@
+resource "heroku_app" "app" {
+  name = var.app_name
+  region = "eu"
+}
+
+# Build code & release to the app
+resource "heroku_build" "app" {
+  app = heroku_app.app.name
+  source = {
+    url =  "https://github.com/fabriziomiano/skylabs-be-ex01/archive/feature/automation.tar.gz"
+    version = "1.0"
+  }
+}
+
+# Launch the app's web process by scaling-up
+resource "heroku_formation" "app" {
+  app        = heroku_app.app.name
+  type       = "web"
+  quantity   = 1
+  size       = "hobby"
+  depends_on = [heroku_build.app]
+}
